fac4868f836fe73b3579b8458549d2f16e4c3c46
mido.kh.rashed@gmail.com <mido.kh.rashed@gmail.com>
2026-04-28 20:59:43 +0000

    Add Bronze layer with config-based data ingestion
    Initialize Bronze layer ingestion using configurable extraction logic.
    Reads CSV files and loads them into Delta tables using PySpark.

---
+++ code/bronze/bronze_config.py
@@ -0,0 +1,52 @@
+extract_config = [
+
+    
+    {
+        'table_name': 'bronze.crm_cust_info',
+        'path': '/Volumes/workspace/bronze/source_systems/source_crm/cust_info.csv',
+        'mode': 'overwrite',
+        'source' : 'crm'
+
+
+    },
+    {
+        'table_name': 'bronze.crm_prd_info',
+        'path': '/Volumes/workspace/bronze/source_systems/source_crm/prd_info.csv',
+        'mode': 'overwrite',
+        'source' : 'crm'
+
+    },
+
+    {
+        'table_name': 'bronze.crm_sales_details',
+        'path': '/Volumes/workspace/bronze/source_systems/source_crm/sales_details.csv',
+        'mode': 'overwrite',
+        'source' : 'crm'
+
+    },
+
+    {
+        'table_name': 'bronze.erp_cust_az12',
+        'path': '/Volumes/workspace/bronze/source_systems/source_erp/CUST_AZ12.csv',
+        'mode': 'overwrite',
+        'source' : 'erp'
+
+    },
+    {
+        'table_name': 'bronze.erp_loc_a101',
+        'path': '/Volumes/workspace/bronze/source_systems/source_erp/LOC_A101.csv',
+        'mode': 'overwrite',
+        'source' : 'erp'
+
+    },
+    {
+        'table_name': 'bronze.erp_px_cat_g1v2',
+        'path': '/Volumes/workspace/bronze/source_systems/source_erp/PX_CAT_G1V2.csv',
+        'mode': 'overwrite',
+        'source' : 'erp'
+
+    },
+
+]
+
+
